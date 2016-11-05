3a1f31b83bcf693ce0f6d2ba1fb1c6b8eb554578
Jamie Bliss <astronouth7303@gmail.com>
2016-11-05 14:24:38 -0400

    Frame out a MongoDB api

--- red/routes.py
+++ red/routes.py
@@ -1,30 +1,37 @@
 from . import application
 from flask_restful import Resource, Api
+from flask.ext.pymongo import PyMongo
+
+mongo = PyMongo(application)
 
 @application.route('/')
 def index():
-	...
+    ...
 
 api = Api(application)
 
 class Project(Resource):
-	def get(self, projid):
-		return {'hello': 'world'}
+    def get(self, projid):
+        return mongo.db.projects.find_one_or_404({'_id': projid})
 
-	def put(self, projid):
-		...
+    def put(self, projid):
+        mongo.db.projects.replace_one({'_id': projid}, request.form)
+        return '', 201
 
-	def delete(self, projid):
-		...
+    def delete(self, projid):
+        mongo.db.projects.delete_one({'_id': projid})
+        return '', 204
 
 api.add_resource(Project, '/projects/<string:projid>')
 
 
 class ProjectList(Resource):
-	def get(self):
-		...
+    def get(self):
+        return mongo.db.projects.find()
 
-	def post(self):
-		...
+    def post(self):
+        res = mongo.db.projects.insert_one(request.form)
+        # FIXME: Created
+        return res.inserted_id, 201
 
 api.add_resource(ProjectList, '/projects')
