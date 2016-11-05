e5a1748a035b4c19306bcbea51c4e0a69a947809
Jamie Bliss <astronouth7303@gmail.com>
2016-11-05 15:49:28 -0400

    Got something started

--- red/__init__.py
+++ red/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 __all__ = 'application',
 
 application = Flask('red', instance_relative_config=True)
-application.config.from_object('red.default_settings')
 application.config.from_pyfile('application.cfg', silent=True)
 
 # Just get these imported
--- red/routes.py
+++ red/routes.py
@@ -1,14 +1,22 @@
 from . import application
+from flask import render_template, url_for
 from flask_restful import Resource, Api
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 mongo = PyMongo(application)
 
 @application.route('/')
 def index():
-    ...
+    return render_template('index.html')
 
 api = Api(application)
+
+def jsonify_doc(doc):
+    return {
+        k: str(v) if isinstance(v, ObjectId) else v
+        for k,v in doc.items()
+    }
 
 class Project(Resource):
     def get(self, projid):
@@ -16,22 +24,24 @@ class Project(Resource):
 
     def put(self, projid):
         mongo.db.projects.replace_one({'_id': projid}, request.form)
-        return '', 201
+        return '', 204
 
     def delete(self, projid):
         mongo.db.projects.delete_one({'_id': projid})
         return '', 204
 
-api.add_resource(Project, '/projects/<string:projid>')
+api.add_resource(Project, '/projects/<projid>')
 
 
 class ProjectList(Resource):
     def get(self):
-        return mongo.db.projects.find()
+        return [
+            jsonify_doc(doc)
+            for doc in mongo.db.projects.find()
+        ]
 
     def post(self):
         res = mongo.db.projects.insert_one(request.form)
-        # FIXME: Created
-        return res.inserted_id, 201
+        return '', 201, {'Location': url_for('.Project', projid=res.inserted_id)}
 
 api.add_resource(ProjectList, '/projects')
