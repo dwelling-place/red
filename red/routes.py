c505607fe09e7b7ae71c6a3a396cb6aa29f65a01
Jamie Bliss <astronouth7303@gmail.com>
2016-11-06 11:20:40 -0500

    Add CSV download

--- red/routes.py
+++ red/routes.py
@@ -1,5 +1,8 @@
+import csv
+import io
+from collections import OrderedDict
 from . import application
-from flask import render_template, request
+from flask import render_template, request, make_response
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -46,3 +49,40 @@ class ProjectList(Resource):
         return loc, 201, {'Location': loc}
 
 api.add_resource(ProjectList, '/projects')
+
+FIELD_DISPLAY_NAMES = OrderedDict([
+    ('name', 'Project'),
+    ('address', 'Address'),
+    ('start_date', 'Start Date'),
+    ('end_date', 'End Date'),
+    ('stage', 'Stage'),
+    ('completion', 'Completion'),
+    ('psh', 'PSH'),
+    ('livework', 'Live/Work'),
+    ('affordable', 'Aff. Housing'),
+    ('commercial', 'Commercial'),
+    ('comments', 'Comments/Updates'),
+])
+
+def value_display(k, v):
+    if not v:
+        return v
+    elif k == 'completion':
+        return float(v) * 100
+
+@application.route('/csv')
+def csvdownload():
+    buf = io.StringIO()
+    dw = csv.DictWriter(buf, FIELD_DISPLAY_NAMES.values());
+    dw.writeheader()
+    for doc in mongo.db.projects.find():
+        dw.writerow({
+            FIELD_DISPLAY_NAMES[k]: value_display(k, v) 
+            for k,v in doc.items() if k in FIELD_DISPLAY_NAMES})
+
+
+    r = make_response(buf.getvalue())
+    r.headers['Content-Type'] = 'text/csv'
+    r.headers['Content-Disposition'] = 'attachment; filename=RED.csv'
+
+    return r
